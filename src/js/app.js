85faa892a3d4d206e68f92d56f93323ed3e4f895
ruvick <rudikov.web@ya.ru>
2024-04-06 15:04:49 +0300

    Стилизовал скроллбар

--- src/js/app.js
+++ src/js/app.js
@@ -64,7 +64,7 @@ flsFunctions.menuInit();
 Документация: https://template.fls.guru/template-docs/funkcional-popup.html
 Сниппет (HTML): pl
 */
-// import './libs/popup.js'
+import './libs/popup.js'
 
 /*
 Модуль параллакса мышью
