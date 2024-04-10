3e0c2efaae8853d03521bb3d51079bf985521e9e
ruvick <rudikov.web@ya.ru>
2024-04-10 11:50:05 +0300

    Activities catalog спойлеры

--- src/js/app.js
+++ src/js/app.js
@@ -93,7 +93,7 @@ import * as flsForms from "./files/forms/forms.js";
 // flsForms.formRating();
 
 /* Модуль работы с select. */
-// import './libs/select.js'
+import './libs/select.js'
 
 /* (В работе) Модуль работы с масками.*/
 /*
