b441add3a1d760b6b857ce7bef7837a506d061ba
ruvick <rudikov.web@ya.ru>
2024-04-10 12:07:43 +0300

    Сборка

--- src/js/files/forms/range.js
+++ src/js/files/forms/range.js
@@ -8,35 +8,86 @@ import * as noUiSlider from 'nouislider';
 // import 'nouislider/dist/nouislider.css';
 
 export function rangeInit() {
-	const priceSlider = document.querySelector('#range');
-	if (priceSlider) {
-		let textFrom = priceSlider.getAttribute('data-from');
-		let textTo = priceSlider.getAttribute('data-to');
-		noUiSlider.create(priceSlider, {
-			start: 0, // [0,200000]
-			connect: [true, false],
-			range: {
-				'min': [0],
-				'max': [200000]
-			}
+
+	const rangeItems = document.querySelectorAll('[data-range]');
+
+	if (rangeItems.length) {
+		rangeItems.forEach(rangeItem => {
+			const fromValue = rangeItem.querySelector('[data-range-from]');
+			const toValue = rangeItem.querySelector('[data-range-to]');
+			const stepsSlider = rangeItem.querySelector('[data-range-item]');
+			let inputs = [fromValue, toValue];
+			noUiSlider.create(stepsSlider, {
+				start: [Number(fromValue.value), Number(toValue.value)], // [0,200000]
+				connect: true,
+				// tooltips: [true, true],
+				range: {
+					'min': [Number(fromValue.dataset.rangeFrom)],
+					'max': [Number(toValue.dataset.rangeTo)]
+				}
+			});
+
+			stepsSlider.noUiSlider.on('update', function (values, handle) {
+				inputs[handle].value = values[handle];
+				// });
+
+				// item.noUiSlider.on('update', function (values, handle) {
+				// 	fromValue.value = values[handle];
+				// 	toValue.value = values[handle];
+
+			});
+			// Listen to keydown events on the input field.
+			inputs.forEach(function (input, handle) {
+
+				input.addEventListener('change', function () {
+					stepsSlider.noUiSlider.setHandle(handle, this.value);
+				});
+
+				input.addEventListener('keydown', function (e) {
+
+					let values = stepsSlider.noUiSlider.get();
+					let value = Number(values[handle]);
+					let steps = stepsSlider.noUiSlider.steps();
+					let step = steps[handle];
+					let position;
+
+					switch (e.which) {
+
+						case 13:
+							stepsSlider.noUiSlider.setHandle(handle, this.value);
+							break;
+
+						case 38:
+							position = step[1];
+							if (position === false) {
+								position = 1;
+							}
+
+							if (position !== null) {
+								stepsSlider.noUiSlider.setHandle(handle, value + position);
+							}
+
+							break;
+
+						case 40:
+
+							position = step[0];
+
+							if (position === false) {
+								position = 1;
+							}
+
+							if (position !== null) {
+								stepsSlider.noUiSlider.setHandle(handle, value - position);
+							}
+
+							break;
+					}
+				});
+			});
 		});
-		/*
-		const priceStart = document.getElementById('price-start');
-		const priceEnd = document.getElementById('price-end');
-		priceStart.addEventListener('change', setPriceValues);
-		priceEnd.addEventListener('change', setPriceValues);
-		*/
-		function setPriceValues() {
-			let priceStartValue;
-			let priceEndValue;
-			if (priceStart.value != '') {
-				priceStartValue = priceStart.value;
-			}
-			if (priceEnd.value != '') {
-				priceEndValue = priceEnd.value;
-			}
-			priceSlider.noUiSlider.set([priceStartValue, priceEndValue]);
-		}
 	}
 }
 rangeInit();
+
+
