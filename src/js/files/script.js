4053df8f9b8cd1599d7220e304f0a7889fa27dd3
ruvick <rudikov.web@ya.ru>
2024-05-01 17:10:21 +0300

    д-адаптив guides acatalog

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -56,6 +56,13 @@ window.onload = function () {
 	let numberGuests = document.querySelector('.number-guests');
 	let sidebarItemOver = document.querySelector('.sidebar-template__item--over');
 
+	const selectOptions = document.querySelectorAll('.select__option');
+	const itemResultText = document.querySelector('.item-result__text');
+	const itemResult = document.querySelector('.item-result');
+	const resultBtn = document.querySelector('.item-result__btn');
+	const select = document.querySelector('.form');
+	const selectContent = document.querySelector('.select__content');
+
 	if (inputGuests) {
 		inputGuests.addEventListener('input', function () {
 			if (inputGuests.value > 1) {
@@ -72,24 +79,16 @@ window.onload = function () {
 				}
 			}, 1500);
 		});
-	}
 
-
-	const selectOptions = document.querySelectorAll('.select__option');
-	const itemResultText = document.querySelector('.item-result__text');
-	const itemResult = document.querySelector('.item-result');
-	const resultBtn = document.querySelector('.item-result__btn');
-	const select = document.querySelector('.form');
-	const selectContent = document.querySelector('.select__content');
-
-	selectOptions.forEach(option => {
-		option.addEventListener('click', function () {
-			itemResultText.textContent = option.textContent;
-			itemResult.classList.add('_active');
-			select.value = option.getAttribute('data-value');
-			selectContent.textContent = option.textContent;
+		selectOptions.forEach(option => {
+			option.addEventListener('click', function () {
+				itemResultText.textContent = option.textContent;
+				itemResult.classList.add('_active');
+				select.value = option.getAttribute('data-value');
+				selectContent.textContent = option.textContent;
+			});
 		});
-	});
+	}
 
 	if (resultBtn) {
 		resultBtn.addEventListener("click", function (e) {
@@ -105,92 +104,7 @@ window.onload = function () {
 		});
 	}
 
-
 }
 //========================================================================================================================================================
 
 
-
-//========================================================================================================================================================
-
-// gsap.registerPlugin(MotionPathPlugin, ScrollTrigger);
-
-// gsap.set("#motionSVG", { scale: 0.7, autoAlpha: 1 });
-// gsap.set("#tractor", { transformOrigin: "50% 50%" });
-// let rotateTo = gsap.quickTo("#tractor", "rotation"),
-// 	prevDirection = 0;
-
-// gsap.to("#motionSVG", {
-// 	scrollTrigger: {
-// 		trigger: "#motionPath",
-// 		start: "top center",
-// 		end: () => "+=" + document.querySelector("#motionPath").getBoundingClientRect().height,
-// 		scrub: 0.5,
-// 		markers: true,
-// 		onUpdate: self => {
-// 			if (prevDirection !== self.direction) { // only run this when we're changing direction
-// 				rotateTo(self.direction === 1 ? 0 : -180);
-// 				prevDirection = self.direction;
-// 			}
-// 		}
-// 	},
-// 	ease: pathEase("#motionPath"), // a custom ease that helps keep the tractor centered
-// 	immediateRender: true,
-// 	motionPath: {
-// 		path: "#motionPath",
-// 		align: "#motionPath",
-// 		alignOrigin: [0.5, 0.5],
-// 		autoRotate: 90,
-// 	}
-// });
-
-// function pathEase(path, config = {}) {
-// 	let axis = config.axis || "y",
-// 		precision = config.precision || 1,
-// 		rawPath = MotionPathPlugin.cacheRawPathMeasurements(MotionPathPlugin.getRawPath(gsap.utils.toArray(path)[0]), Math.round(precision * 12)),
-// 		useX = axis === "x",
-// 		start = rawPath[0][useX ? 0 : 1],
-// 		end = rawPath[rawPath.length - 1][rawPath[rawPath.length - 1].length - (useX ? 2 : 1)],
-// 		range = end - start,
-// 		l = Math.round(precision * 200),
-// 		inc = 1 / l,
-// 		positions = [0],
-// 		a = [],
-// 		minIndex = 0,
-// 		smooth = [0],
-// 		minChange = (1 / l) * 0.6,
-// 		smoothRange = config.smooth === true ? 7 : Math.round(config.smooth) || 0,
-// 		fullSmoothRange = smoothRange * 2,
-// 		getClosest = p => {
-// 			while (positions[minIndex] <= p && minIndex++ < l) { }
-// 			a.push((p - positions[minIndex - 1]) / (positions[minIndex] - positions[minIndex - 1]) * inc + minIndex * inc);
-// 			smoothRange && a.length > smoothRange && (a[a.length - 1] - a[a.length - 2] < minChange) && smooth.push(a.length - smoothRange);
-// 		},
-// 		i = 1;
-// 	for (; i < l; i++) {
-// 		positions[i] = (MotionPathPlugin.getPositionOnPath(rawPath, i / l)[axis] - start) / range;
-// 	}
-// 	positions[l] = 1;
-// 	for (i = 0; i < l; i++) {
-// 		getClosest(i / l);
-// 	}
-// 	a.push(1); // must end at 1.
-// 	if (smoothRange) { // smooth at the necessary indexes where a small difference was sensed. Make it a linear change over the course of the fullSmoothRange
-// 		smooth.push(l - fullSmoothRange + 1);
-// 		smooth.forEach(i => {
-// 			let start = a[i],
-// 				j = Math.min(i + fullSmoothRange, l),
-// 				inc = (a[j] - start) / (j - i),
-// 				c = 1;
-// 			i++;
-// 			for (; i < j; i++) {
-// 				a[i] = start + inc * c++;
-// 			}
-// 		});
-// 	}
-// 	return p => {
-// 		let i = p * l,
-// 			s = a[i | 0];
-// 		return i ? s + (a[Math.ceil(i)] - s) * (i % 1) : 0;
-// 	}
-// }
