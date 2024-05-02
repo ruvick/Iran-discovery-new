ba2ef845cb6cd4f9e821bb8e23f61b2e522e5856
ruvick <rudikov.web@ya.ru>
2024-05-02 17:22:17 +0300

    Перенастройка календаря

--- src/js/files/air-datepicker.js
+++ src/js/files/air-datepicker.js
@@ -1,9 +1,8 @@
 // Подключение функционала
 import AirDatepicker from 'air-datepicker';
+
 // Подключение стилей
 import 'air-datepicker/air-datepicker.css';
-
-// import localeEn from 'air-datepicker/locale/en';
 //========================================================================================================================================================
 
 // new AirDatepicker('#input', {
@@ -11,43 +10,68 @@ import 'air-datepicker/air-datepicker.css';
 // 	multipleDatesSeparator: ' - '
 // });
 
-let buttonAir = {
-	content: 'درخواست کنید',
-	className: 'custom-button-classname',
-	onClick: (dp) => {
-		let date = new Date('2021-07-26');
-		dp.selectDate(date);
-		dp.setViewDate(date);
-	}
-}
+// let buttonAir = {
+// 	content: 'درخواست کنید',
+// 	className: 'custom-button-classname',
+// 	onClick: (dp) => {
+// 		let date = new Date('2021-07-26');
+// 		dp.selectDate(date);
+// 		dp.setViewDate(date);
+// 	}
+// }
 
-new AirDatepicker('.detail-calendar', {
-	// locale: localeEn,
-	range: true,
-	multipleDatesSeparator: ' - ',
-	buttons: [buttonAir, 'clear']
-})
+// new AirDatepicker('.detail-calendar', {
+// 	// locale: localeEn,
+// 	range: true,
+// 	multipleDatesSeparator: ' - ',
+// 	buttons: [buttonAir, 'clear']
+// })
 
 new AirDatepicker('.tour-booking-calendar', {
-	// inline: true
-	language: 'en',
 	range: true,
 	multipleDatesSeparator: ' - ',
+	buttons: [
+		{
+			content: 'درخواست کنید',
+			className: 'custom-button-classname',
+			onClick: (dp) => {
+				dp.selectDate(new Date());
+			}
+		},
+		'clear'
+	],
+	locale: {
+		days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+		daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+		daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+		months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+		monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+		today: 'درخواست کنید',
+		clear: 'پرتاب کردن',
+		dateFormat: 'MM/dd/yyyy',
+		timeFormat: 'hh:mm aa',
+		firstDay: 0
+	}
 })
 
-new AirDatepicker('.hotel-reservation-calendar', {
-	range: true,
-	language: 'en',
-	multipleDatesSeparator: ' - ',
-})
+// new AirDatepicker('.hotel-reservation-calendar', {
+// 	range: true,
+// 	language: 'en',
+// 	multipleDatesSeparator: ' - ',
+// })
 
-new AirDatepicker('.hotel-reservation-calendar-2', {
-	range: true,
-	multipleDatesSeparator: ' - ',
-})
+// new AirDatepicker('.hotel-reservation-calendar-2', {
+// 	range: true,
+// 	multipleDatesSeparator: ' - ',
+// })
 
-new AirDatepicker('.guide-calendar', {
-	range: true,
-	multipleDatesSeparator: ' - ',
-})
+// new AirDatepicker('.guide-calendar', {
+// 	range: true,
+// 	multipleDatesSeparator: ' - ',
+// })
 
+
+// $('.tour-booking-calendar').datepicker({
+// 	language: 'en',
+// 	minDate: new Date()
+// })
